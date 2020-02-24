1c72775d33057ff1a8ec64b073add1ed4f218fe3
pintugithub10 <workingpintu@gmail.com>
2020-02-24 08:56:23 +0530

    All File Added

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test{
+public static void main(String[] args){
+	for(int i=1;i<21;i++)
+	System.out.print(i+", ");
+
+int s=0;
+for(int i=1;i<21;i++)
+s+=i;
+System.out.println("Sum = "+s);
+
+int f=1;
+for(int i=2;i<=5;i++)
+f*=i;
+System.out.println("Factorial  = "+s);
+
+
+} 
+
+}
---
+++ Test1.java
@@ -0,0 +1,34 @@
+public class Test1{
+public static void main(String[] args){
+	for(int i=1;i<21;i++)
+	System.out.print(i+", ");
+
+int s=0;
+for(int i=1;i<21;i++)
+s+=i;
+System.out.println("Sum = "+s);
+
+int f=1;
+for(int i=2;i<=5;i++)
+f*=i;
+System.out.println("Factorial  = "+f);
+
+
+
+for(int i=1;i<21;i++)
+	System.out.print(i+", ");
+
+int s1=0;
+for(int i=1;i<21;i++)
+s1+=i;
+System.out.println("Sum = "+s1);
+
+int f1=1;
+for(int i=2;i<=5;i++)
+f1*=i;
+System.out.println("Factorial  = "+f1);
+
+
+} 
+
+}
